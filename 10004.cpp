d50aeadfb13ca82a7658d214b344ffdf57107da3
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2020-03-09 18:19:33 +0600

    Update 10004.cpp

--- 10004.cpp
+++ 10004.cpp
@@ -14,15 +14,15 @@ void BFS(int s, map< int, vector< int > >G)
     while(!q.empty())
     {
         string x;
-        
+   
         int top = q.front();
         
         if(color[top] == "r"){
-			x = "b";
-		}
-		else{
-			x = "r";
-		}
+		x = "b";
+	}
+	else{
+		x = "r";
+	}
 		
         for(int i=0; i<(int)G[top].size(); i++)
         {
@@ -41,28 +41,26 @@ void BFS(int s, map< int, vector< int > >G)
 int main()
 {
 	int totVertice;
-    int edges;
-    while( (scanf("%d",&totVertice) == 1) && totVertice != 0 && (scanf("%d",&edges)== 1))
-    {
-        map< int,vector< int > >G;
-        int source; // for this problem code source = 0
-        for(int i=0; i<edges; i++)
-        {
-            int x, y;
-            scanf("%d %d", &x, &y);
-            if(i == 0){
-				source = x;
-			}
-            G[x].push_back(y);
-            G[y].push_back(x);
-        }
-        
-        
-        BFS(source,G);
-        string t1;
-        int flag = 0;
-        map< int ,int >:: iterator i;
-        for(i = visited.begin();i != visited.end();i++){
+    	int edges;
+    	while( (scanf("%d",&totVertice) == 1) && totVertice != 0 && (scanf("%d",&edges)== 1))
+    	{
+		map< int,vector< int > >G;
+		int source; // for this problem code source = 0
+		for(int i=0; i<edges; i++)
+		{
+		    int x, y;
+		    scanf("%d %d", &x, &y);
+		    if(i == 0){
+					source = x;
+				}
+		    G[x].push_back(y);
+		    G[y].push_back(x);
+		}
+		BFS(source,G);
+		string t1;
+		int flag = 0;
+		map< int ,int >:: iterator i;
+		for(i = visited.begin();i != visited.end();i++){
 			//cout<< color[i->first]<< " : ";
 			t1 = color[i->first];
 			for( int j = 0; j<(int)G[i->first].size() ; j++){
@@ -70,14 +68,14 @@ int main()
 					flag = 1;
 				}
 				//cout<< color[G[i->first][j]] <<' ';
-					
+
 			}
 			//cout<<endl;
 		}
 		G.clear();
 		visited.clear();
 		color.clear();
-		
+
 		if(flag){
 			cout<<"NOT BICOLORABLE."<<endl;
 		}else{
