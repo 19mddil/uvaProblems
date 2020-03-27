fbb3c347ed5ef206ba99ff9305280bb9623a4f1e
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2020-03-26 18:51:33 -0700

    Create 10783.cpp

---
+++ 10783.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void){
+	int tt;
+	scanf("%d",&tt);
+	int count= 0;
+	while(tt--){
+		count++;
+		int a,b;
+		scanf("%d %d",&a,&b);
+		if(a&1){
+			printf("Case %d: %d\n",count,( ((b+1)/2)*((b+1)/2) )- ( (a/2)*(a/2) ) );
+		}else{
+			printf("Case %d: %d\n",count,( ((b+1)/2)*((b+1)/2) )- ( ((a+1)/2)*((a+1)/2) ) );
+		}
+	}
+}
